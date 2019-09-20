fb2afef1cf76426aaeff26b6e840b165406c8d75
Bernadette Davis <bsd231@nyu.edu>
2019-09-20 15:43:26 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 3) do
+ActiveRecord::Schema.define(version: 2) do
 
   create_table "artists", force: :cascade do |t|
     t.string "name"
@@ -21,8 +21,6 @@ ActiveRecord::Schema.define(version: 3) do
   end
 
   create_table "songs", force: :cascade do |t|
-    t.string "name"
-    t.string "genre"
   end
 
 end
